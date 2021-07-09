fccb3e99cad2d3396404ed49f61eb8f7e8c49e99
Ismael-devTR <dev.tristan.romero@gmail.com>
2021-07-08 20:15:31 -0500

    RoR: using turbolinks, real-time conections

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :courses do
+    resources :lessons
+  end
+
+  resources :lessons, only: [] do
+    resources :questions
+  end
+
+  root to: "courses#index"
 end
